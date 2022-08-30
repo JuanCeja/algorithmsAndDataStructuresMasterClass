8ce77d836f6e118c3c386e1b56449ba924be0561
JuanCeja <jmc347ls2@yahoo.com>
2022-08-30 16:41:32 -0700

    initial commit

---
+++ SlidingWindow/maxSubArraySum/maxSubArraySum.js
@@ -0,0 +1,52 @@
+// write a function called maxSubArraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array.
+
+// let arr1 =  [1,2,5,2,8,1,5]
+// let num1 = 2;
+// output = 10
+
+// let arr2 =  [1,2,5,2,8,1,5]
+// let num2 = 4;
+// output = 17
+
+// let arr3 =  [4,2,1,6]
+// let num3 = 1;
+// output = 6
+
+// let arr4 =  [4,2,1,6,2]
+// let num4 = 4;
+// output = 13
+
+// let arr5 =  []
+// let num5 = 4;
+// output = null
+
+// =================================================================================================================
+
+let arr1 = [1, 2, 5, 2, 8, 1, 5];
+let num1 = 2;
+
+let arr2 = [1, 2, 5, 2, 8, 1, 5];
+let num2 = 4;
+
+let arr3 = [4, 2, 1, 6];
+let num3 = 1;
+
+let arr4 = [4, 2, 1, 6, 2];
+let num4 = 4;
+
+let arr5 = [];
+let num5 = 4;
+
+var maxSubarraySum = function (arr, num) {
+    
+};
+
+console.log(maxSubarraySum(arr1, num1));
+
+console.log(maxSubarraySum(arr2, num2));
+
+console.log(maxSubarraySum(arr3, num3));
+
+console.log(maxSubarraySum(arr4, num4));
+
+console.log(maxSubarraySum(arr5, num5));
