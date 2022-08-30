16f8d02569b3384cef749a743ead029c53711e63
JuanCeja <jmc347ls2@yahoo.com>
2022-08-30 14:38:20 -0700

    initial commit

---
+++ validAnagram/validAnagram.js
@@ -0,0 +1,44 @@
+// given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman. 
+
+// sample 1: ' ' => ' ' true
+// sample 2: aaz => zza false
+// sample 3: 'anagram' => 'nagaram' true
+// sample 4: 'rat' => 'car' false
+// sample 5: 'qwerty' => 'qeywrt' true
+// sample 6: 'texttwisttime' => 'timetwisttext' true
+// sample 7: 'awesome' => 'awesom' false
+
+// =========================================================================
+
+let string1 = ' ';
+let string2 = ' ';
+
+let string3 = 'aaz';
+let string4 = 'zza';
+
+let string5 = 'anagram';
+let string6 = 'nagaram';
+
+let string7 = 'rat';
+let string8 = 'car';
+
+let string9 = 'awesome';
+let string10 = 'awesom';
+
+let string11 = 'qwerty';
+let string12 = 'qeywrt';
+
+let string13 = 'texttwisttime';
+let string14 = 'timetwisttext';
+
+var validAnagram = function(s1, s2) {
+
+};
+
+console.log(validAnagram(string1, string2));
+console.log(validAnagram(string3, string4));
+console.log(validAnagram(string5, string6));
+console.log(validAnagram(string7, string8));
+console.log(validAnagram(string9, string10));
+console.log(validAnagram(string11, string12));
+console.log(validAnagram(string13, string14));
