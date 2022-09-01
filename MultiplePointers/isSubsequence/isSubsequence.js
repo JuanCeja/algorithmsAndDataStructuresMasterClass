2c991d60e39ba2bc66aac8a8925401676174e28d
JuanCeja <jmc347ls2@yahoo.com>
2022-09-01 13:02:19 -0700

    complete w/ O(N+M) time comp and O(1) space comp.

--- MultiplePointers/isSubsequence/isSubsequence.js
+++ MultiplePointers/isSubsequence/isSubsequence.js
@@ -18,14 +18,34 @@
 
 //=============================================================================================================
 
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+    // create pointer and empty string variable
+    let pointer = 0;
+    let testString = "";
 
+    // loop through second string 
+    for(let val in str2) {
+
+        // compare str2 element to str1. if a match push to element to empty string
+        if(str2[val] === str1[pointer]) {
+            testString += str2[val];
+            
+            // move pointer up 
+            pointer++;
+        }
+    }
+
+    // compare str1 to our string variable if matches return true
+    // else return false
+    if(testString === str1) {
+        return true
+    } else return false;
 };
 
-isSubsequence('hello', 'hello world'); // true
+console.log(isSubsequence('hello', 'hello world')); // true
 
-isSubsequence('sing', 'sting'); // true
+console.log(isSubsequence('sing', 'sting')); // true
 
-isSubsequence('abc', 'abracadabra'); // true
+console.log(isSubsequence('abc', 'abracadabra')); // true
 
-isSubsequence('abc', 'acb'); // false (order matters)
+console.log(isSubsequence('abc', 'acb')); // false (order matters)
