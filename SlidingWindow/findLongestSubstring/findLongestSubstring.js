57a9ddad068fee90a27d703219793227d8c5e4e1
JuanCeja <jmc347ls2@yahoo.com>
2022-09-06 14:40:04 -0700

    completed

--- SlidingWindow/findLongestSubstring/findLongestSubstring.js
+++ SlidingWindow/findLongestSubstring/findLongestSubstring.js
@@ -15,14 +15,37 @@
 
 // ===================================================================================
 
-function findLongestSubstring(str){
+function findLongestSubstring(s){
     
+    let max = 0;
+    let begin = 0;
+    let set = new Set();
+
+    for(let end = 0; end < s.length; end++){
+
+        let char = s[end];
+
+        while(set.has(char)) {
+            set.delete(s[begin]);
+            begin++;
+        }
+        set.add(char);
+        max = Math.max(max, end - begin + 1);
+    }
+    return max;
 };
 
-console.log(findLongestSubstring('')) // 0
-console.log(findLongestSubstring('rithmschool')) // 7
-console.log(findLongestSubstring('thisisawesome')) // 6
-console.log(findLongestSubstring('thecatinthehat')) // 7
-console.log(findLongestSubstring('bbbbbb')) // 1
-console.log(findLongestSubstring('longestsubstring')) // 8
-console.log(findLongestSubstring('thisishowwedoit')) // 6
+console.log(findLongestSubstring('')) 
+// 0
+console.log(findLongestSubstring('rithmschool')) 
+// 7
+console.log(findLongestSubstring('thisisawesome')) 
+// 6
+console.log(findLongestSubstring('thecatinthehat')) 
+// 7
+console.log(findLongestSubstring('bbbbbb')) 
+// 1
+console.log(findLongestSubstring('longestsubstring')) 
+// 8
+console.log(findLongestSubstring('thisishowwedoit')) 
+// 6
