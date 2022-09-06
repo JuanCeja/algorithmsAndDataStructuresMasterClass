e1d09d783985a259ef31d74ed6f55eddced1d2dd
JuanCeja <jmc347ls2@yahoo.com>
2022-09-06 13:58:49 -0700

    initial commit

---
+++ SlidingWindow/findLongestSubstring/findLongestSubstring.js
@@ -0,0 +1,28 @@
+// Sliding Window - findLongestSubstring
+// Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
+
+// samples
+
+// findLongestSubstring('') // 0
+// findLongestSubstring('rithmschool') // 7
+// findLongestSubstring('thisisawesome') // 6
+// findLongestSubstring('thecatinthehat') // 7
+// findLongestSubstring('bbbbbb') // 1
+// findLongestSubstring('longestsubstring') // 8
+// findLongestSubstring('thisishowwedoit') // 6
+
+// Time Complexity - O(n)
+
+// ===================================================================================
+
+function findLongestSubstring(str){
+    
+};
+
+console.log(findLongestSubstring('')) // 0
+console.log(findLongestSubstring('rithmschool')) // 7
+console.log(findLongestSubstring('thisisawesome')) // 6
+console.log(findLongestSubstring('thecatinthehat')) // 7
+console.log(findLongestSubstring('bbbbbb')) // 1
+console.log(findLongestSubstring('longestsubstring')) // 8
+console.log(findLongestSubstring('thisishowwedoit')) // 6
