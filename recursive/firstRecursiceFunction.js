89784bca2e6a567bc035605af13b2d8a02f7e931
JuanCeja <jmc347ls2@yahoo.com>
2022-09-07 13:57:55 -0700

    initial commit

--- recursive/firstRecursiceFunction.js
+++ recursive/firstRecursiceFunction.js
@@ -7,3 +7,16 @@
 // We want to call the same function over and over but each time with a different piece of data. You don't want to keep using the same data because we will never get anywhere. So we change our data and run our data again. 
 
 // ===================================================================================
+
+function countDown(num) {
+    if(num <= 0) {
+        console.log('All done!');
+        return;
+    } 
+
+    console.log(num);
+    num--;
+    countDown(num);
+}
+
+console.log(countDown(3))
