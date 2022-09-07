65766b39c52658e4dcb5e3740ca4f4188f00d1b8
JuanCeja <jmc347ls2@yahoo.com>
2022-09-07 13:56:03 -0700

    initial commit

---
+++ recursive/firstRecursiceFunction.js
@@ -0,0 +1,9 @@
+// 1. BASE CASE
+
+// We need a base case. This is the condition when the recursion ends. This is the most important concept to understand.
+
+// 2. DIFFERENT INPUT
+
+// We want to call the same function over and over but each time with a different piece of data. You don't want to keep using the same data because we will never get anywhere. So we change our data and run our data again. 
+
+// ===================================================================================
