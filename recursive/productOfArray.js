8e88bb992b5c81b8c2561aee9896b62997ddb9b4
JuanCeja <jmc347ls2@yahoo.com>
2022-09-08 13:10:06 -0700

    initial commit

---
+++ recursive/productOfArray.js
@@ -0,0 +1,21 @@
+// productOfArray
+
+// Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+
+// productOfArray([1,2,3]) output 6
+// productOfArray([1,2,3,10]) output 60
+
+// =====================================================================================
+
+function productOfArray(arr) {
+    // base case
+    
+    // different input
+};
+
+
+console.log(productOfArray([1,2,3]))
+// output 6
+
+console.log(productOfArray([1,2,3,10]))
+// output 60
