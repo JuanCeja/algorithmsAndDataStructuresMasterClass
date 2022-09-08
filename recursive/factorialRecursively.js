74617c8117bbae2b0fb59d525c70dce090df2557
JuanCeja <jmc347ls2@yahoo.com>
2022-09-08 15:02:44 -0700

    added description

--- recursive/factorialRecursively.js
+++ recursive/factorialRecursively.js
@@ -6,6 +6,10 @@
 
 // We want to call the same function over and over but each time with a different piece of data. You don't want to keep using the same data because we will never get anywhere. So we change our data and run our data again. 
 
+// FACTORIAL
+
+// Write a function factorial which accepts a number and returns the factorial of that number. A factorial is the product of an integer and all the integers below it; e.g., factorial four ( 4! ) is equal to 24, because 4 * 3 * 2 * 1 equals 24.  factorial zero (0!) is always 1.
+
 // ===================================================================================
 
 function factorial (num) {
