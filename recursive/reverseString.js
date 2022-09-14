6327ea60825d9371c9f1f4391a17a253bc467458
JuanCeja <jmc347ls2@yahoo.com>
2022-09-13 23:19:45 -0700

    initial commit

---
+++ recursive/reverseString.js
@@ -0,0 +1,22 @@
+// reverse
+
+// Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
+
+// =====================================================================================
+
+function reverse(str) {
+    // base case
+    if(str.length <= 1) return str;
+
+    // different input
+    return reverse(str.slice(1)) + str[0]
+};
+
+console.log(reverse('awesome'))
+// output: 'emosewa'
+
+console.log(reverse('rithmschool'))
+// output: 'loohcsmhtir'
