0ebee56fca6018abeb5e9c6e3e25f892f530eae8
JuanCeja <jmc347ls2@yahoo.com>
2022-09-22 14:16:45 -0700

    completed solution

--- recursive/flatten.js
+++ recursive/flatten.js
@@ -10,14 +10,31 @@
 // ===========================================================
 
 function flatten(arr) {
-    // base case
-    if(arr.length === 0) return [];
+    // create our empty array to fill
+    let flattened = [];
 
-    // recursive call with different input
-    return flatten(arr.slice(1)).concat(arr[0])
+    // loop through arr
+    for(let i = 0; i < arr.length; i++){
+
+        // current iteration variable
+        let element = arr[i];
+        
+        // if element is an array we recall our recursive call with different input
+        if(Array.isArray(element)){
+            flattened = flattened.concat(flatten(element));
+
+        // else we push our element to our empty flattened array
+        } else {
+            flattened.push(element);
+        }
+
+    }
+
+    // return our flattened array
+    return flattened;
 }
 
 console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
-// console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
-// console.log(flatten([[1],[2],[3]])) // [1,2,3]
-// console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3
+console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+console.log(flatten([[1],[2],[3]])) // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
