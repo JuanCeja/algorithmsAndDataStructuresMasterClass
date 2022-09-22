09bfe116207efd5be2312bf4fcf56610be53b5f9
JuanCeja <jmc347ls2@yahoo.com>
2022-09-22 12:40:27 -0700

    initial commit

---
+++ recursive/flatten.js
@@ -0,0 +1,19 @@
+// flatten
+
+// Write a recursive function called flatten which accepts an array of arrays and returns a new array with all values flattened.
+
+// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3]
+
+// ===========================================================
+
+function flatten() {
+
+}
+
+console.log(flatten([1, 2, 3, [4, 5] ])) // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+console.log(flatten([[1],[2],[3]])) // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3
