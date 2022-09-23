cb64c1b415a323fafd811a8781438f5504897f3f
JuanCeja <jmc347ls2@yahoo.com>
2022-09-23 14:31:23 -0700

    completed solution with commented steps

--- recursive/capitalizeWords.js
+++ recursive/capitalizeWords.js
@@ -8,8 +8,18 @@
 // =====================================================================
 let words = ["i", "am", "learning", "recursion"];
 
-function capitalizeWords() {
-    
+function capitalizeWords(arr) {
+    let newArray = [];
+
+    // base case
+    if(arr.length === 1) return arr[0].toUpperCase();
+
+    // here we add our first element to our newArray capitalized
+    newArray.push(arr[0].toUpperCase());
+
+    // recursive call with different input
+    // we concat to newArray so we dont lose our current elements and remake our recursive call on our sliced arr
+    return newArray.concat(capitalizeWords(arr.slice(1)));
 }
 
 console.log(capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
