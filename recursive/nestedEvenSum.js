ad8967bc243f0fe1e08a39170c7ef5245d9618ca
JuanCeja <jmc347ls2@yahoo.com>
2022-09-23 13:19:24 -0700

    pseudo code steps

--- recursive/nestedEvenSum.js
+++ recursive/nestedEvenSum.js
@@ -24,8 +24,19 @@ var obj2 = {
   e: { e: { e: 2 }, ee: "car" },
 };
 
-function nestedEvenSum() {
-  // add whatever parameters you deem necessary - good luck!
+function nestedEvenSum(obj) {
+    // create variable to add even numbers
+
+    // loop through object
+
+    // check to see if element in the loop in an object
+
+    // if it is an object we make our recursive call
+
+    // if it is not a object we check to see if the value is even
+    // if value is even we add to our evenCount variable
+
+    // return our evenCount variable
 }
 
 nestedEvenSum(obj1); // 6
