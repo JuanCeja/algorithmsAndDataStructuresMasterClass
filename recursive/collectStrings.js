f810ca07081640e7df716415075ff4cf810042a5
JuanCeja <jmc347ls2@yahoo.com>
2022-09-26 16:53:45 -0700

    completed with commented step by stepss

--- recursive/collectStrings.js
+++ recursive/collectStrings.js
@@ -22,14 +22,26 @@ const obj = {
 
 function collectStrings (obj) {
     // array that will hold our strings
+    let stringsArray = [];
 
     // loop through obj
+    for(let key in obj) {
 
-    // if value of key is a string we push it into array
+        // if value of key is a string we push it into array
+        if(typeof obj[key] === "string") {
+            stringsArray.push(obj[key]);
 
-    // else if value of key is an obj typeOf we make our recursive call
-    
+        } else if(typeof obj[key] === "object"){
+            // else if value of key is an obj typeOf we make our recursive call
+
+            // we concat because we dont want to lose our data in our current array. once we make our recursion call stringsArray will be an empty array again so we concat to keep our previous data. 
+            
+            return stringsArray.concat(collectStrings(obj[key]));
+        }
+    };
+
     // return our array of strings
+    return stringsArray;
 };
 
 console.log(collectStrings(obj)); // ["foo", "bar", "baz"])
