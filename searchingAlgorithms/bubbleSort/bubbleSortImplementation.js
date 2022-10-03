ea1a7a6c347be35602eebd78427d7b72420ea7e2
JuanCeja <jmc347ls2@yahoo.com>
2022-10-03 16:01:11 -0700

    pseudo code steps

---
+++ searchingAlgorithms/bubbleSort/bubbleSortImplementation.js
@@ -0,0 +1,9 @@
+function bubbleSort(arr){
+    
+    // loop through our arr
+    // our inner loop to compare
+    // if our first element is greater than second element we swap 
+    // return our sorted array
+}
+
+console.log(bubbleSort([8, 4, 6, 2, 9, 1, 23]));
