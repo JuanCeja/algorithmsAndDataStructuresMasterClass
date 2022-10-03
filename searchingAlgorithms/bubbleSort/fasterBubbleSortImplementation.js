fa6ef4181799e7f2c29e5c9f806474e849700d34
JuanCeja <jmc347ls2@yahoo.com>
2022-10-03 16:54:47 -0700

    initial commit

--- searchingAlgorithms/bubbleSort/fasterBubbleSortImplementation.js
+++ searchingAlgorithms/bubbleSort/fasterBubbleSortImplementation.js
@@ -1,9 +1,12 @@
 function bubbleSort(arr){
     // loop through our arr
-    for(let i = 0; i < arr.length; i++){
-
+    // WE GO BACKWARDS in our array so we have a smaller value of which is i
+    for(let i = arr.length; i > 0; i--){
         // this will be our inner array. it will execute through every iteration of our first loop
-        for(let j = 0; j < arr.length; j++) {
+        // NOW WE HAVE LESS COMPARISONS to compare because i is a smaller value. we have no longer have to go through every comparison again that have already been compared. this reduces our comparisons
+
+        for(let j = 0; j < i - 1; j++) {
+
             // if our current element is greater than our next element we swap them
             if(arr[j] > arr[j + 1]){
                 let temp = arr[j];
