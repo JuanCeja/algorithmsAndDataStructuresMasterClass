6412acaf3ab1c79d8bc186107189fe71e4883c5d
JuanCeja <jmc347ls2@yahoo.com>
2022-10-04 14:35:52 -0700

    added an if statement to avoid unnecessary swaps

--- selectionSort/optimizedSelectionSort.js
+++ selectionSort/optimizedSelectionSort.js
@@ -1,5 +1,32 @@
 function selectionSort(arr){
-    
+
+    // create our outer loop
+    for(let i = 0; i < arr.length; i++) {
+
+        // create our inner loop to compare the remaining elements to our current element in our outer loop
+        for(let j = i + 1; j < arr.length; j++) {
+
+            // set our minimum var to the index of i
+            let minimum = i;
+
+            // we compare our i(mininum) element to our current element(j)
+            if(arr[j] < arr[i]){
+
+                // if j is less than our minimum we reassign minimum
+                minimum = j;
+            }
+
+            // we make our swap from our current minimum to our current element in our otter loop
+            // but only if the indices do not match so we dont make unnecessary swaps
+            if(i !== minimum) {
+                let temp = arr[minimum];
+                arr[minimum] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+        // return our sorted array
+        return arr;
 };
 
 console.log(selectionSort([5,7,3,8,10,17,1]))
