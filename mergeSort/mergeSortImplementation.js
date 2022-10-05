544100c5ca4f5b088e2f79ad7f3af559ef30f284
JuanCeja <jmc347ls2@yahoo.com>
2022-10-05 16:06:49 -0700

    completed

--- mergeSort/mergeSortImplementation.js
+++ mergeSort/mergeSortImplementation.js
@@ -1,15 +1,18 @@
+
+// ========= MERGING FUNCTION ONLY ===========
+
 function merge(arr1, arr2){
     // create our pointer variables
     // create an empty array to push values to
+    let mergedArray = [];
     let i = 0;
     let j = 0;
-    let mergedArray = [];
     
     // use a while loop starting at the beginning of both arrays 
     while(i < arr1.length && j < arr2.length){
 
         // compare values and push the smaller value until one arrays reaches its end
-        if(arr1[i] < arr2[j]){
+        if(arr2[j] > arr1[i]){
             mergedArray.push(arr1[i]);
             i++;
         } else {
@@ -20,7 +23,7 @@ function merge(arr1, arr2){
 
     // make 2 more while loops that will push remaining values
     while(i < arr1.length){
-        mergedArray.push(arr1[1]);
+        mergedArray.push(arr1[i]);
         i++;
     }
     
@@ -33,5 +36,20 @@ function merge(arr1, arr2){
     return mergedArray;
 };
 
-// this will only work if both arrays are already sorted
-console.log(merge([1,10,50], [2,14,50,99,100]));
+// =========== RECURSIVE MERGE SORT HERE ===========
+
+function mergeSort(arr){
+    // base case
+    if(arr.length <= 1) return arr;
+
+    // we find our mid so we can keep splitting
+    let mid = Math.floor(arr.length / 2);
+
+    // we make our recursive calls on every half of the array
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+
+    return merge(left, right)
+}
+
+console.log(mergeSort([10,24,76,73,72,1,9]))
