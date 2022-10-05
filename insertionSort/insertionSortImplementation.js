46279c6cc88362bc4e7e4523264b20cc2c7fde03
JuanCeja <jmc347ls2@yahoo.com>
2022-10-05 13:38:19 -0700

    completed

--- insertionSort/insertionSortImplementation.js
+++ insertionSort/insertionSortImplementation.js
@@ -1,14 +1,12 @@
 function insertionSort(arr) {
     for(let i = 1; i < arr.length; i++){
-
-        let currentVal = arr[i];
-        let index = i;
-
-        for(let j = i - 1; j >= 0 && arr[j] > currentVal; j--){
-            arr[j + 1] = arr[j];
-            index = j
+        for(let j = i; j > 0; j--){
+            if(arr[j] < arr[j-1]){
+                [arr[j], arr[j-1]] = [arr[j-1], arr[j]]
+            } else {
+                break;
+            }
         }
-        arr[index] = currentVal;
     }
     return arr;
 };
