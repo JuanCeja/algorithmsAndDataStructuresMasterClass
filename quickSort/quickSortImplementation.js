d0491af9c706e6784904c4ff77213a1c6b10a7a0
JuanCeja <jmc347ls2@yahoo.com>
2022-10-06 14:53:01 -0700

    pseudo code for the pivot function

---
+++ quickSort/quickSortImplementation.js
@@ -0,0 +1,11 @@
+function pivot(arr,start = 0, end = arr.length - 1){
+    // grab the pivot from the start of the array
+
+    // store the current pivot index in a variable(this will keep track of where the pivot should end up)
+
+    // loop through the array from the start until the end
+
+        // if the pivot is greater than the current element, increment the pivot index variable and then swap the current element with the element at the pivot index
+
+    // swap the starting element(i.e. the pivot) with the pivot index
+};
