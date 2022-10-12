03668529dbef83cf8c3d1001b893229acdb94005
JuanCeja <jmc347ls2@yahoo.com>
2022-10-11 18:54:22 -0700

    completed pivot function

--- quickSort/quickSortImplementation.js
+++ quickSort/quickSortImplementation.js
@@ -8,4 +8,25 @@ function pivot(arr,start = 0, end = arr.length - 1){
         // if the pivot is greater than the current element, increment the pivot index variable and then swap the current element with the element at the pivot index
 
     // swap the starting element(i.e. the pivot) with the pivot index
+
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+      };
+    
+      // We are assuming the pivot is always the first element
+      let pivot = arr[start];
+      let swapIdx = start;
+    
+      for (let i = start + 1; i <= end; i++) {
+        if (pivot > arr[i]) {
+          swapIdx++;
+          swap(arr, swapIdx, i);
+        }
+      }
+    
+      // Swap the pivot from the start the swapPoint
+      swap(arr, start, swapIdx);
+      return swapIdx;
 };
+
+console.log(pivot([4,8,2,1,5,7,6,3]));
