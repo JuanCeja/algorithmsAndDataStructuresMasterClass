7b678599b8178a31295c1ed02ee3d6535337f308
JuanCeja <jmc347ls2@yahoo.com>
2022-10-13 22:38:03 -0700

    completed full quick sort solution

--- quickSort/quickSortImplementation.js
+++ quickSort/quickSortImplementation.js
@@ -29,4 +29,29 @@ function pivot(arr,start = 0, end = arr.length - 1){
       return swapIdx;
 };
 
+function quickSort(arr, left = 0, right = arr.length -1){
+  if(left < right){
+      let pivotIndex = pivot(arr, left, right) //3
+      //left
+      quickSort(arr,left,pivotIndex-1);
+      //right
+      quickSort(arr,pivotIndex+1,right);
+    }
+   return arr;
+} 
+         
+console.log(quickSort([100,-3,2,4,6,9,1,2,5,3,23]))
+
+
+
+
+// [4,6,9,1,2,5,3]
+// [3,2,1,4,6,9,5]
+//        4
+//  3,2,1    6,9,5
+//      3      6
+//  2,1      5  9
+//    2
+//  1
+
 console.log(pivot([4,8,2,1,5,7,6,3]));
