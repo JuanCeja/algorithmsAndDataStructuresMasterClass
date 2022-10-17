a83f313184528eebe72a7485d31fe0d1bf61d6df
JuanCeja <jmc347ls2@yahoo.com>
2022-10-17 14:20:06 -0700

    pseudo code for pivot helper function

---
+++ quickSort/quickSortImplementation.js
@@ -0,0 +1,16 @@
+// ================ PIVOT HELPER FUNCTION ====================
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+
+  // grab the pivot from the start of the array
+
+  // store the current pivot index in a count variable(this will keep track of where the pivot should end up)
+
+  // loop through the array from the start until the end
+  
+    // if the pivot is greater then the current array element, increment the pivot count variable and then swap the current element with the element at the pivot index
+
+  // swap the starting element(i.e. the pivot) index
+
+  // return the pivot index
+};
