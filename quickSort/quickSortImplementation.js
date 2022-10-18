a05c237dd23463ec3faa881d703096be3fcfe8d7
JuanCeja <jmc347ls2@yahoo.com>
2022-10-18 12:48:33 -0700

    starting quick sort function

--- quickSort/quickSortImplementation.js
+++ quickSort/quickSortImplementation.js
@@ -6,18 +6,18 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   let pivot = arr[start];
 
   // store the current pivot index in a variable(this will keep track of where the pivot should end up)
-  let pivotIndex = start;
+  let swapIndex = start;
 
   // loop through the array from the start until the end
   for(let i = 1; i < end + 1; i++) {
 
     // if the pivot is greater then the current array element, increment the pivot index variable and then swap the current element with the element at the pivot index
     if(pivot > arr[i]){
-      pivotIndex++;
+      swapIndex++;
 
       let temp = arr[i];
-      arr[i] = arr[pivotIndex];
-      arr[pivotIndex] = temp;
+      arr[i] = arr[swapIndex];
+      arr[swapIndex] = temp;
     }
 
   }
@@ -26,15 +26,18 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   // we need to swap our current pivot to its correct place
   // we currently have [4,2,1,3,5,7,6,8] after swap we get [3,2,1,4,5,7,6,8]
   // after the swap we put the pivot in its correct place on the array
-  let temp = arr[pivotIndex];
-  arr[pivotIndex] = arr[start];
+  let temp = arr[swapIndex];
+  arr[swapIndex] = arr[start];
   arr[start] = temp;
 
   // return the pivot index
-  return pivotIndex;
+  console.log(arr)
+  return swapIndex;
 
 };
 
-console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]))
+// ======================== QUICK SORT FUNCTION =========================
 
-// this is a comment to show you how this works locs
+
+
+console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]))
