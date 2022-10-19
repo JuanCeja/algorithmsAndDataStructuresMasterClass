1a908b955fd3aa49f53204dabaf9765ab8f55c34
JuanCeja <jmc347ls2@yahoo.com>
2022-10-19 14:53:32 -0700

    initial commit

--- singlyLinkedList/singlyLinkedListPop.js
+++ singlyLinkedList/singlyLinkedListPop.js
@@ -5,13 +5,12 @@
 // piece of data - val
 // reference to the next node - next
 
-class Node{
+class Node {
     constructor(val){
         this.val = val;
         this.next = null;
     }
 }
-
 
 // ============================ THIS IS OUR SINGLY LINKED LIST ===============================
 
@@ -22,31 +21,21 @@ class SinglyLinkedList{
         this.length = 0;
     }
 
-    // this is our push method
     push(val){
-
-        // create a new node using the value passed to the function
         let newNode = new Node(val);
 
-        // if there is no head property on the list, set the head and tail to be the new node created
-        if(!this.head) {
+        if(this.head === null){
             this.head = newNode;
-            this.tail = this.head;
+            this.tail = newNode;
         } else {
-            // otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
             this.tail.next = newNode;
             this.tail = newNode;
         }
 
-        // increment the length by one
         this.length++;
-        return this;
+    }
+
+    pop(){
+
     }
 }
-
-let list = new SinglyLinkedList();
-list.push('Hello');
-list.push('There');
-list.push('Juan');
-
-console.log(list);
