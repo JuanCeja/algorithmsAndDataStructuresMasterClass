52b85202e3f4255c1cc9a5b457323699043c27d8
JuanCeja <jmc347ls2@yahoo.com>
2022-10-20 15:46:41 -0700

    initial commit

--- singlyLinkedList/singlyLinkedListGet.js
+++ singlyLinkedList/singlyLinkedListGet.js
@@ -15,16 +15,16 @@ class Node {
 // ============================ THIS IS OUR SINGLY LINKED LIST ===============================
 
 class SinglyLinkedList {
-    constructor() {
+    constructor(){
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
 
-    push(val) {
+    push(val){
         let newNode = new Node(val);
 
-        if(this.head === null) {
+        if(!this.head) {
             this.head = newNode;
             this.tail = newNode;
         } else {
@@ -57,51 +57,30 @@ class SinglyLinkedList {
         if(!this.head) return undefined;
 
         let prevHead = this.head;
-
         this.head = prevHead.next;
         this.length--;
         if(this.length === 0) {
             this.head = null;
             this.tail = null;
         }
-        return prevHead
+        return prevHead;
     }
 
-    unshift(val){
-        // create a new node using the value passed to the function 
+    unShift(val){
         let newNode = new Node(val);
 
-        // if there is no head property on the list, set the head and tail to be the newly created node
         if(!this.head) {
             this.head = newNode;
             this.tail = this.head;
         } else {
-
-            // we had this else statement because if we dont / this.head.next will keep on pointing back to its head
-
-            // set the head property on the list to be that newly created node
             newNode.next = this.head;
             this.head = newNode;
         }
-
-        // increment the length by 1
         this.length++;
-
-        // return the linked list
         return this;
     }
+
+    get(val) {
+        
+    }
 }
-
-let list = new SinglyLinkedList();
-
-list.push('Hello');
-list.push('there');
-list.push('Juan');
-
-console.log('before unshift', list);
-list.unshift('Unshifted to the beginning')
-
-console.log('===============================================')
-console.log('===============================================')
-
-console.log('after unshift', list);
