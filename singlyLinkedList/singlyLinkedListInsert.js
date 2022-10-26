07742999884482982661d2b71660c658517f1a63
JuanCeja <jmc347ls2@yahoo.com>
2022-10-25 20:06:32 -0700

    complete

--- singlyLinkedList/singlyLinkedListInsert.js
+++ singlyLinkedList/singlyLinkedListInsert.js
@@ -106,27 +106,46 @@ class SinglyLinkedList {
         }
 
         insert(index, value){
+
+            // this is our new node we will be inserting
+            let newNode = new Node(value);
+
             // if the index is less than zero or greater than the length, return false
+            if(index < 0 || index > this.length) return false;
+            
 
             // if the index is the same as the length, PUSH a new node to the end of the list
-
+            if (index === this.length) return !!this.push(value)
+            
             // if the index is 0, UNSHIFT a new node to the start of the list
-
+            if(index === 0) return !!this.unShift(value)
+            
             // otherwise , using the GET method, access the node at the index - 1
+            let prev = this.get(index - 1);
+            let temp = prev.next;
 
             // set the next property on that node to be the new node
+            prev.next = newNode;
 
             // set the next property on the new node to be the previous next 
-            
+            newNode.next = temp;
+                
             // increment the length
+            this.length++
 
             // return true
+            return true;
         }
 }
 
 let list = new SinglyLinkedList();
-list.push("I'm head :)");
-list.push('Hello');
-list.push('!');
-list.push('<3');
-list.push("I'm tail :)");
+
+list.push(100);
+list.push(201);
+list.push(250);
+list.push(350);
+
+console.log(list.insert(0, 'FIRST'));
+console.log(list.insert(4, 'LAST'));
+console.log(list.insert(55, 'LAST'));
+console.log(list.insert(-4, 'LAST'));
