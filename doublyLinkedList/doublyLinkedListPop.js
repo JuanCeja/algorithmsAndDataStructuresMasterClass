5435da9c02b9beea4637bed8215bcb1df7c4ceb2
JuanCeja <jmc347ls2@yahoo.com>
2022-11-08 15:37:53 -0800

    complete doubly linked list pop

--- doublyLinkedList/doublyLinkedListPop.js
+++ doublyLinkedList/doublyLinkedListPop.js
@@ -30,17 +30,41 @@ class DoublyLinkedList {
 
     pop(){
         // if there is no head, return undefined
+        if(this.head === null) return undefined;
 
         // store the current tail in a variable to return later
+        let poppedNode = this.tail;
 
         // if the length is 1, set the head and tail to be null
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            // update the tail to be the previous node
+            this.tail = poppedNode.prev;
 
-        // update the tail to be the previous node
+            // set the newTail's next to be null
+            this.tail.next = null;
 
-        // set the newTail's next to be null
+            // remove all ties of our poppedNode
+            poppedNode.next = null;
+            poppedNode.prev = null;
+        }
 
         // decrement the length
+        this.length--;
 
         // return the removed value
+        return poppedNode;
     }
 }
+
+let list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list);
