e2ce1ae9f13360307f3e52742c2a61aee1541ad8
JuanCeja <jmc347ls2@yahoo.com>
2022-11-09 13:34:54 -0800

    initial commit

--- doublyLinkedList/doublyLinkedListShift.js
+++ doublyLinkedList/doublyLinkedListShift.js
@@ -2,7 +2,7 @@ class Node {
     constructor(val) {
         this.val = val;
         this.next = null;
-        this.tail = null;
+        this.prev = null;
     }
 }
 
@@ -28,34 +28,24 @@ class DoublyLinkedList {
         return this;
     }
 
-    pop(){
-        // if there is no head, return undefined
-        if(this.head === null) return undefined;
-
-        // store the current tail in a variable to return later
+    pop() {
+        if (this.length === 0) return undefined;
         let poppedNode = this.tail;
-
-        // if the length is 1, set the head and tail to be null
-        if(this.length === 1) {
+        if (this.length === 1) {
             this.head = null;
             this.tail = null;
         } else {
-            // update the tail to be the previous node
             this.tail = poppedNode.prev;
-
-            // set the newTail's next to be null
             this.tail.next = null;
-
-            // remove all ties of our poppedNode
-            poppedNode.next = null;
             poppedNode.prev = null;
+            poppedNode.next = null;
         }
-
-        // decrement the length
         this.length--;
-
-        // return the removed value
         return poppedNode;
+    }
+
+    shift(){
+        
     }
 }
 
