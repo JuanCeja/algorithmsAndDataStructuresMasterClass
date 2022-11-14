84efefdd510108c4dadbfce2b96839cdb27786d9
JuanCeja <jmc347ls2@yahoo.com>
2022-11-14 13:17:42 -0800

    pseudo code for doubly linked list REMOVE

--- doublyLinkedList/doublyLinkedListRemove.js
+++ doublyLinkedList/doublyLinkedListRemove.js
@@ -119,7 +119,21 @@ class DoublyLinkedList {
     }
 
     remove(idx){
-        
+        // if the index is less than zero or greater than or equal to the length return undefined
+
+        // if the index is 0, SHIFT
+
+        // if the index is the same as the length - 1, POP
+
+        // use the GET method to retrieve the item to be removed
+
+        // update the next and prev properties to remove the found node from the list
+
+        // set the next and prev to null on the found node
+
+        // decrement the length
+
+        // return the removed node
     }
 }
 
