b5069ab901d5c986867a3e73827665232a1d4dae
JuanCeja <jmc347ls2@yahoo.com>
2022-11-15 13:28:30 -0800

    pseudo code for STACK push singly linked list

--- stacks/singlyLinkedListStackImplementation.js
+++ stacks/singlyLinkedListStackImplementation.js
@@ -15,6 +15,17 @@ class Stack {
     }
 
     push(val){
-        
+        // the function should accept a value
+        // create a new node with that value
+
+        // if there are no nodes in the stack, set the first and the last property to be the new node
+
+        // if there is at least one node, create a variable that stores the current first property on the stack
+
+        // reset the first property to be the newly created node 
+
+        // set the next property on the node to be the previously created variable
+
+        // increment the size of the stack by 1
     }
 }
