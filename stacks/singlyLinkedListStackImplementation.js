555b3961e399416c0ab3ddd62e7ffd20b001436f
JuanCeja <jmc347ls2@yahoo.com>
2022-11-15 13:47:52 -0800

    initial commit for pop

--- stacks/singlyLinkedListStackImplementation.js
+++ stacks/singlyLinkedListStackImplementation.js
@@ -1,26 +1,26 @@
 // we want to start of by creating our Stack class and Node class
 
 class Node {
-    constructor(val){
+    constructor(val) {
         this.val = val;
         this.next = null;
     }
 }
 
 class Stack {
-    constructor(){
+    constructor() {
         this.first = null;
         this.last = null;
         this.size = 0;
     }
 
-    push(val){
+    push(val) {
         // the function should accept a value
         // create a new node with that value
         let newNode = new Node(val);
 
         // if there are no nodes in the stack, set the first and the last property to be the new node
-        if(this.size === 0) {
+        if (this.size === 0) {
             this.first = newNode;
             this.last = newNode;
         } else {
@@ -39,4 +39,14 @@ class Stack {
         // we can also do all that in 1 line
         // return ++this.size;
     }
+
+    pop() {
+        
+    }
 }
+
+let stack = new Stack();
+stack.push('first');
+stack.push('second');
+stack.push('third');
+console.log(stack);
