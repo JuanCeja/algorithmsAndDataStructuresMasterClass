796692f2e01c4dfbfaf2dfaa5342485c75647e8e
JuanCeja <jmc347ls2@yahoo.com>
2022-11-15 13:40:27 -0800

    complete STACK singly linked list PUSH

--- stacks/singlyLinkedListStackImplementation.js
+++ stacks/singlyLinkedListStackImplementation.js
@@ -17,15 +17,23 @@ class Stack {
     push(val){
         // the function should accept a value
         // create a new node with that value
+        let newNode = new Node(val);
 
         // if there are no nodes in the stack, set the first and the last property to be the new node
+        if(this.size === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            // if there is at least one node, create a variable that stores the current first property on the stack
+            let tempFirst = this.first;
 
-        // if there is at least one node, create a variable that stores the current first property on the stack
+            // reset the first property to be the newly created node 
+            this.first = newNode;
 
-        // reset the first property to be the newly created node 
-
-        // set the next property on the node to be the previously created variable
-
+            // set the next property on the node to be the previously created variable
+            newNode.next = tempFirst;
+        }
         // increment the size of the stack by 1
+        this.size++;
     }
 }
