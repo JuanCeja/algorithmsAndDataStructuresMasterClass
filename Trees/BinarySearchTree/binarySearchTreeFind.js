591d8a32ff7be0471942b65ecd026d9713fe996e
JuanCeja <jmc347ls2@yahoo.com>
2022-11-17 15:08:45 -0800

    complete

--- Trees/BinarySearchTree/binarySearchTreeFind.js
+++ Trees/BinarySearchTree/binarySearchTreeFind.js
@@ -38,21 +38,49 @@ class BinarySearchTree {
 
     find(val) {
         // start at the root
+        let current = this.root;
 
         // check if there is a root, if not were done return false
+        if (this.root === null) return false;
 
-        // if there is a root, check if the value of the new node is the value we are looking for
-        // if found were done
+        while (true) {
 
-        // if it is greater
-            // check to see if there is a node to the right
-                // if there is, move to that node and repeate these steps
-                // if there is not, were done searching
+            if (current.val === val) return true;
 
-        // if it is less
-            // check to see if there is a node to the left
-                // if there is, move to that node and repeat these steps
-                // if there is not, were done searching
+            // if there is a root, check if the value of the new node is the value we are looking for
+            if (this.root.val === val) {
+                // if found were done
+                return true;
+            } else {
+
+                // if it is greater
+                if (val > current.val) {
+
+                    // check to see if there is a node to the right
+                    if (current.right) {
+
+                        // if there is, move to that node and repeat these steps
+                        current = current.right
+                    } else {
+
+                        // if there is not, were done searching
+                        return false
+                    }
+                } else {
+                    // if it is less
+                    // check to see if there is a node to the left
+                    if (current.left) {
+
+                        // if there is, move to that node and repeat these steps
+                        current = current.left;
+                    } else {
+                        // if there is not, were done searching
+                        return false;
+                    }
+
+                }
+            }
+        }
     }
 }
 
@@ -64,4 +92,4 @@ tree.insert(19);
 tree.insert(15);
 tree.insert(5);
 tree.insert(3);
-console.log(tree);
+console.log(tree.find(23));
