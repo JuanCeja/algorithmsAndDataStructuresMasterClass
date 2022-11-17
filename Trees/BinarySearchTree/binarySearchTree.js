af80e9772bfc8222a7bfda0d0767e95a47c92dae
JuanCeja <jmc347ls2@yahoo.com>
2022-11-17 13:56:55 -0800

    complete

--- Trees/BinarySearchTree/binarySearchTree.js
+++ Trees/BinarySearchTree/binarySearchTree.js
@@ -12,55 +12,39 @@ class BinarySearchTree {
     }
 
     insert(val) {
-        // create new node
         let newNode = new Node(val);
-
-        // starting at the root
-        let current = this.root;
-
-        // check if there is a root, if not - the root now becomes that new node.
-        if (!this.root) {
+        if (this.root === null) {
             this.root = newNode;
-        } else {
-
-            // if there is a root, check if the value of the new node is greater than or less than the value
-            // if it is greater
-            if(newNode.val > current.val) {
-                // check to see if there is a node to the right
-                // if there is, move to that node and repeat these steps
-                while(current.right) {
-                    current = current.right;
-
-                    // if there is not, add that node as the right property
-                    if(!current.right) {
-                        current.right = newNode;
-                        return this;
-                    }
-                }
-
-            } else {
-                // if it is less than
-                // check to see if there is a node to the left
-                // if there is, move to that node and repeat these steps
-                while(current.left) {
-                    current = current.left;
-                }
-                // if there is not, add that node as the left property
-                if(!current.left) {
+            return this;
+        }
+        let current = this.root;
+        while (true) {
+            if (val === current.val) return undefined;
+            if (val < current.val) {
+                if (current.left === null) {
                     current.left = newNode;
                     return this;
+                } else {
+                    current = current.left
+                }
+            } else if (val > current.val) {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
+                } else {
+                    current = current.right;
                 }
             }
-
         }
-
     }
 }
 
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
-
-console.log(tree);
+console.log(tree)
+console.log(tree.insert(10));
+console.log(tree.insert(5));
+console.log(tree.insert(13));
+console.log(tree.insert(11));
+console.log(tree.insert(2));
+console.log(tree.insert(16));
+console.log(tree.insert(7));
