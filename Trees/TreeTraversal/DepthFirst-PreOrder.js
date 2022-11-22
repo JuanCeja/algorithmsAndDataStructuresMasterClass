e81938b45853ff643957d7aa4d35114ecf9cb2a4
JuanCeja <jmc347ls2@yahoo.com>
2022-11-22 13:37:23 -0800

    pseudo code for Depth First Pre Order

--- Trees/TreeTraversal/DepthFirst-PreOrder.js
+++ Trees/TreeTraversal/DepthFirst-PreOrder.js
@@ -84,7 +84,21 @@ class BinarySearchTree {
     }
 
     DepthFirstPreOrder() {
+        // create a variable to store the values of nodes visited
         
+        // store the root of the BST in a variable called current
+
+        // write a helper function(traverse) which accepts a node
+
+            // push the value of the node to the variables that stores the values
+
+            // if the node has a left property, call the helper function with the left property on the node
+
+            // if the node has a right property, call the helper function with the right property on the node
+
+        // invoke the helper function with the current variable
+
+        // return the array of values
     }
 }
 
