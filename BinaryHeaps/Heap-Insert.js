93521903756a33898ec5e48502e0a46d18a64b51
JuanCeja <jmc347ls2@yahoo.com>
2022-11-23 14:36:02 -0800

    pseudo code for MaxBinaryHeap-Insert

--- BinaryHeaps/Heap-Insert.js
+++ BinaryHeaps/Heap-Insert.js
@@ -5,6 +5,16 @@ class MaxBinaryHeap {
     }
 
     insert(val) {
-        // 
+        // push the value into the values property on the heap
+
+        // Bubble up:
+            // create a variable called index which is the length of the values property -1
+
+            // create a variable called parentIndex which is the floor of (index - 1) / 2
+
+            // keep looping as long as the values element at the parentIndex is less than the values element at the child index
+                // swap the value of the values element at the parentIndex with the value of the element property at the child index
+
+                // set the index to be the parentIndex, and start over
     }
 }
