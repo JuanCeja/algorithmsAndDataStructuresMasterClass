3200d9e8cfa9ec37e09fa87cfd731a7cd3950e31
JuanCeja <jmc347ls2@yahoo.com>
2022-11-29 15:34:01 -0800

    creating helper function for extract max

--- BinaryHeaps/Heap-Extract-Max.js
+++ BinaryHeaps/Heap-Extract-Max.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeap {
     constructor() {
-        this.values = [41, 39, 33, 18, 27, 12];
+        this.values = [];
     }
 
     insert(val) {
@@ -24,30 +24,25 @@ class MaxBinaryHeap {
     }
 
     extractMax() {
-        // swap the first value in the values property with the last one
+        const max = this.values[0];
+        const end = this.values.pop();
+        this.values[0] = end;
+        this.sinkDown();
 
-        // pop from the values property, so you can return the value at the end
+        return max;
+    }
 
-        // have the new root "sink down" to the correct spot
-
-            // your parent index start at 0(the root)
-
-            // find the index of the left child: 2 * idx + 1 (make sure its not out of bounds)
-
-            // find the index of the right child: 2 * idx + 2 (make sure its not out of bounds)
-
-            // if the left or the right child is greater than the element... swap. If both left and right children are larger, swap with the largest child
-
-            // the child index you swapped to now becomes the new parent index
-
-            // keep looking and swapping until neither child is larger than the element
-
-            // return the old root
+    sinkDown() {
+        
     }
 }
 
 let heap = new MaxBinaryHeap();
-heap.insert(55);
-heap.insert(1);
-heap.insert(45);
-console.log(heap.insert(199));
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+console.log(heap.insert(55));
+console.log(heap.extractMax());
