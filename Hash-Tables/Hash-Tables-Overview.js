ff39af231e5e23cf2ea61a50fecbf2d4bf5fd477
JuanCeja <jmc347ls2@yahoo.com>
2022-12-05 13:44:19 -0800

    hash tables

---
+++ Hash-Tables/Hash-Tables-Overview.js
@@ -0,0 +1,7 @@
+// ==================== HASH TABLES IN THE WILD =====================
+
+// Python has dictionaries
+// JS has Objects and Maps. Objects have some restrictions, but are basically hash tables. In OBJECTS you are only allowed to use strings as keys. 
+// Java, Go, and Scala have Maps.
+// Ruby has Hashes
+
