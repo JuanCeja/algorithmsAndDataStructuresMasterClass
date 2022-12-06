f1b1d97e257da3004663f73e56042642a85ff9b6
JuanCeja <jmc347ls2@yahoo.com>
2022-12-06 15:26:55 -0800

    complete

---
+++ Graphs/Graphs-Introduction.js
@@ -0,0 +1,35 @@
+// ======================================================== WHAT ARE GRAPHS ==========================================================
+
+// A Graph is a set of nodes together with a set of pairs of these vertices, which are the connections. So we have a bunch of nodes, and then connections between them. 
+// A Graph is a type of tree, but in a Graph there is no parent node, there is no starting place, there is no entry point. There just a bunch of nodes treated equally with connections between them.
+
+// Heres a good link for Graph Theory: https://www.youtube.com/playlist?list=PLpXOY-RxVRTPPVLBP6-sz6CMWxhtrI-v_
+
+
+
+// ====================================================== ESSENTIAL GRAPH TERMS ======================================================
+
+// VERTEX - it's just a node
+// EDGE - the connection between the nodes
+// WEIGHTED/UNWEIGHTED - values assigned to distances between vertices
+// DIRECTED/UNDIRECTED - directions assigned to distanced and between vertices
+
+
+
+// ======================================================== USES FOR GRAPHS ==========================================================
+
+// Social Networks, Location/Mapping, Routing Algorithms, Visual Hierarchy, File System Optimization, they are EVERYWHERE!
+
+
+
+// ========================================================= TYPES OF GRAPHS =========================================================
+
+// UNDIRECTED GRAPH   -   There is no direction to or polarity to the edges. They are 2 way connections.
+// DIRECTED GRAPH     -   Is often represented with arrows, which indicates the direction, the polarity of that edge. 
+// UNWEIGHTED GRAPH   -   Each connection(edge) between the vertices(nodes) has no value associated with it.
+// WEIGHTED GRAPH     -   Each connection(edge) between the vertices(nodes) has a value associated with it. When we assign a value to the 
+//                        edges, it becomes a weighted graph. There is information about the connection itself
+
+
+
+// 
