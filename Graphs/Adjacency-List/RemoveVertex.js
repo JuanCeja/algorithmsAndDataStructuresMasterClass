3781261c9476a101be77906965e2f393858806c1
JuanCeja <jmc347ls2@yahoo.com>
2022-12-07 15:22:08 -0800

    removeVertex complete

--- Graphs/Adjacency-List/RemoveVertex.js
+++ Graphs/Adjacency-List/RemoveVertex.js
@@ -15,13 +15,35 @@ class Graph {
     removeEdge(vertex1, vertex2) {
         this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => v !== vertex2);
         this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => v !== vertex1);
-    }
+    };
 
     removeVertex(vertex) {
-        // the function should loop as long as there are any other vertices in the adjacency list for that vertex
+        // the function should loop as long as there are any other vertices in the array of that specific vertex
+        while(this.adjacencyList[vertex].length) {
 
-            // call our removeEdge function with the vertex we are removing and any values in the adjacency list for that vertex
+            // call our removeEdge function with the vertex we are removing and any values in the adjacency list for that vertex. we are severing all ties to other vertices
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        
+        // delete the key in the adjacency list for that vertex. delete the empty array
+        delete this.adjacencyList[vertex];
+    };
+};
 
-            // delete the key in the adjacency list for that vertex
-    }
-}
+let g = new Graph();
+g.addVertex('Dallas');
+g.addVertex('Tokyo');
+g.addVertex('Aspen');
+g.addVertex('Los Angeles');
+g.addVertex('Hong Kong');
+g.addEdge('Dallas', 'Tokyo');
+g.addEdge('Dallas', 'Aspen');
+g.addEdge('Hong Kong', 'Tokyo');
+g.addEdge('Hong Kong', 'Dallas');
+g.addEdge('Los Angeles', 'Hong Kong');
+g.addEdge('Los Angeles', 'Aspen');
+console.log(g);
+g.removeVertex('Hong Kong');
+g.removeVertex('Aspen');
+console.log(g);
