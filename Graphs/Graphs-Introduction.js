16ed8e13d5cde82e14132297bfd76d67e0bc06d8
JuanCeja <jmc347ls2@yahoo.com>
2022-12-06 16:06:06 -0800

    updated notes on graphs introduction

--- Graphs/Graphs-Introduction.js
+++ Graphs/Graphs-Introduction.js
@@ -1,6 +1,7 @@
 // ======================================================== WHAT ARE GRAPHS ==========================================================
 
 // A Graph is a set of nodes together with a set of pairs of these vertices, which are the connections. So we have a bunch of nodes, and then connections between them. 
+
 // A Graph is a type of tree, but in a Graph there is no parent node, there is no starting place, there is no entry point. There just a bunch of nodes treated equally with connections between them.
 
 // Heres a good link for Graph Theory: https://www.youtube.com/playlist?list=PLpXOY-RxVRTPPVLBP6-sz6CMWxhtrI-v_
@@ -27,9 +28,19 @@
 // UNDIRECTED GRAPH   -   There is no direction to or polarity to the edges. They are 2 way connections.
 // DIRECTED GRAPH     -   Is often represented with arrows, which indicates the direction, the polarity of that edge. 
 // UNWEIGHTED GRAPH   -   Each connection(edge) between the vertices(nodes) has no value associated with it.
-// WEIGHTED GRAPH     -   Each connection(edge) between the vertices(nodes) has a value associated with it. When we assign a value to the 
-//                        edges, it becomes a weighted graph. There is information about the connection itself
+// WEIGHTED GRAPH     -   Each connection(edge) between the vertices(nodes) has a value associated with it. When we assign a value to 
+//                        the edges, it becomes a weighted graph. There is information about the connection itself.
 
 
 
-// 
+// ======================================================= WAYS TO STORE GRAPHS =======================================================
+
+// ADJACENCY MATRIX    -    A matrix is just a 2 dimensional structure, usually implemented with nested arrays, but not always. 
+//                          Basically we store information in rows and columns so we could represent the connections were using.
+
+// ADJACENCY LIST      -    In a Adjacency List we use an array or list to store the edges. So if we wanted to figure out what edges, 
+//                          what connections there were between node3 and it's other nodes, we would go to the index of node3 in our 
+//                          array and in that index theres a list(array) there that contains the connections. Also, if our nodes are not 
+//                          numeric and let's say they are strings. Well, we could use a hash table, use a key value pair of data 
+//                          structure. We store a given key and attach a list(array) as a value with the connections stored in the 
+//                          array. 
