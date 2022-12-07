1ff944d364d8f9a90817282f794fc3b3c6f0c099
JuanCeja <jmc347ls2@yahoo.com>
2022-12-07 15:00:03 -0800

    pseudo code for removeVertex from graph adjacency list

---
+++ Graphs/Adjacency-List/RemoveVertex.js
@@ -0,0 +1,27 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    };
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    };
+
+    addEdge(v1, v2) {
+        if (this.adjacencyList[v1]) this.adjacencyList[v1].push(v2);
+        if (this.adjacencyList[v2]) this.adjacencyList[v2].push(v1);
+    };
+
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => v !== vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => v !== vertex1);
+    }
+
+    removeVertex(vertex) {
+        // the function should loop as long as there are any other vertices in the adjacency list for that vertex
+
+            // call our removeEdge function with the vertex we are removing and any values in the adjacency list for that vertex
+
+            // delete the key in the adjacency list for that vertex
+    }
+}
