de16604baaa7108be046392b2e3f532849e9bda8
JuanCeja <jmc347ls2@yahoo.com>
2022-12-07 14:04:44 -0800

    addEdge complete

--- Graphs/Adjacency-List/AddEdge.js
+++ Graphs/Adjacency-List/AddEdge.js
@@ -9,10 +9,23 @@ class Graph {
         }
     };
 
-    addEdge (vertix1, vertix2) {
+    addEdge (v1, v2) {
         // the function should find in the adjacency list the key of vertex1 and push vertex2 to the array
-
-        // the function should find in the adjacency list the key of vertex2 and push vertex1 to the array
+        if(this.adjacencyList[v1]) {
+            this.adjacencyList[v1].push(v2);
+        }
         
+        // the function should find in the adjacency list the key of vertex2 and push vertex1 to the array
+        if(this.adjacencyList[v2]) {
+            this.adjacencyList[v2].push(v1);
+        }
     };
 }
+
+let g = new Graph();
+g.addVertex('Dallas');
+g.addVertex('Tokyo');
+g.addVertex('Aspen');
+g.addEdge('Dallas', 'Tokyo');
+g.addEdge('Dallas', 'Aspen');
+console.log(g);
