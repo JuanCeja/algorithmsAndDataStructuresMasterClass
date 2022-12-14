ad67d61cb32085e94319039382216369d9dfae5b
JuanCeja <jmc347ls2@yahoo.com>
2022-12-14 14:39:21 -0800

    pseudo code for approaching dijkstras algorithm

--- Dijkstras-Algorithm/dijkstrasAlgorithm.js
+++ Dijkstras-Algorithm/dijkstrasAlgorithm.js
@@ -15,7 +15,15 @@ class WeightedGraph {
     };
 };
 
+// =============================================================================== THE APPROACH ===============================================================================
 
+// Every time we look to visit a new node, we pick the node with the smallest known distance to visit
+
+// Once we've moved to the node we're going to visit, we look at each of it's neighbors
+
+// For each neighboring node, we calculate the distance by summing the total edges that lead to the node we're checking from the starting node
+
+// If the new total distance to a node is lee than the previous total, we store the shorter distance for that node
 
 let graph = new WeightedGraph();
 graph.addVertex('A');
