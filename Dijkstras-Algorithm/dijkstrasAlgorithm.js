bc48bef666f33ae100e62cce51727494b6006bc2
JuanCeja <jmc347ls2@yahoo.com>
2022-12-14 16:02:41 -0800

    pseudo code for dijkstras algorithm

--- Dijkstras-Algorithm/dijkstrasAlgorithm.js
+++ Dijkstras-Algorithm/dijkstrasAlgorithm.js
@@ -50,15 +50,44 @@ class PriorityQueue {
     };
 }
 
-let q = new PriorityQueue;
-q.enqueue('B', 3);
-q.enqueue('C', 5);
-q.enqueue('D', 2);
-q.enqueue('Q', 20);
-q.enqueue('p', 1);
-console.log(q.values);
-console.log(q.dequeue());
+// =============================================================================== IMPLEMENTATION ===============================================================================
 
+// this function should accept a starting and ending vertex
+
+// create an object(well call it distances) and set each key to be every vertex in the adjacency list with a value of infinity, except for the starting vertex which should have a value of 0
+
+// after setting a value in the distances object, add each vertex with a priority of infinity to the priority queue, except the starting vertex, which should have a priority of 0 because thats were we begin
+
+// create another object called previous and set each key to be every vertex in the adjacency list with a value of null
+
+// start looping as long as there is anything in the priority queue
+
+    // dequeue a vertex from the priority queue
+
+    // if that vertex is the same as the ending vertex - we are done!
+
+    // otherwise loop through each value in the adjacency list at the vertex
+
+        // calculate the distance to that vertex from the starting vertex
+
+        // if the distance is less than what is currently stored in our distances object
+
+            // update the distances object with nex lower distance
+
+            // update the previous object to contain that vertex
+
+            // enqueue the vertex with the total distance from the start node
+
+            
+
+// let q = new PriorityQueue;
+// q.enqueue('B', 3);
+// q.enqueue('C', 5);
+// q.enqueue('D', 2);
+// q.enqueue('Q', 20);
+// q.enqueue('p', 1);
+// console.log(q.values);
+// console.log(q.dequeue());
 
 // let graph = new WeightedGraph();
 // graph.addVertex('A');
