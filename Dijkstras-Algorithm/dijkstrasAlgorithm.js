bed336a527f583efd2eaf0b1a40ca70897a822c4
JuanCeja <jmc347ls2@yahoo.com>
2022-12-26 15:18:09 -0800

    created our weighted graph

--- Dijkstras-Algorithm/dijkstrasAlgorithm.js
+++ Dijkstras-Algorithm/dijkstrasAlgorithm.js
@@ -7,7 +7,7 @@
 
 // For each neighboring node, we calculate the distance by summing the total edges that lead to the node we're checking from the starting node
 
-// If the new total distance to a node is lee than the previous total, we store the shorter distance for that node
+// If the new total distance to a node is less than the previous total, we store the shorter distance for that node
 
 // ====================================================================== PRIORITY QUEUE USING BINARY HEAP =====================================================================
 
--- Dijkstras-Algorithm/dijkstrasRetake.js
+++ Dijkstras-Algorithm/dijkstrasRetake.js
@@ -6,5 +6,36 @@
 
 // For each neighboring node, we calculate the distance by summing the total edges that lead to the node we're checking from the starting node
 
-// If the new total distance to a node is lee than the previous total, we store the shorter distance for that node
+// If the new total distance to a node is less than the previous total, we store the shorter distance for that node
 
+// ========================================================================== SIMPLE WEIGHTED GRAPH ==========================================================================
+
+// we create our weighted graph before we can actually start Dijkstra's Algorithm
+
+// before we can actually find the shortest path in a graph between 2 points, we need to assign values between those points so we can find any path short or long. so we make our graph a WEIGHTED GRAPH. 
+
+class WeightedGraph {
+    constructor() {
+        this.adjacencyList = {};
+    };
+    
+    addVertex (vertex) {
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    };
+
+    addEdge(vertex1, vertex2, weight) {
+        if(this.adjacencyList[vertex1])this.adjacencyList[vertex1].push({node: vertex2, weight});
+        if(this.adjacencyList[vertex2])this.adjacencyList[vertex2].push({node: vertex1, weight});
+    };
+};
+
+
+
+let graph = new WeightedGraph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addEdge('A', 'B', 9);
+graph.addEdge('A', 'C', 5);
+graph.addEdge('B', 'C', 7);
+console.log(graph.adjacencyList);
