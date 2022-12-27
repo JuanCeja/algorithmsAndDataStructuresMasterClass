e6c8263bab4fd6ece9d38d0b5f43a43c0f98e6b9
JuanCeja <jmc347ls2@yahoo.com>
2022-12-27 14:22:12 -0800

    notes on dynamic programming

---
+++ Dynamic-Programming/Dynamic-Programming.js
@@ -0,0 +1,9 @@
+// ================================================================== WHAT IS DYNAMIC PROGRAMMING? ==================================================================
+
+// A method for solving a complex problem by breaking it down into a collection of simpler sub-problems, solving each of those sub-problems just once, and storing their solutions. 
+
+// DYNAMIC PROGRAMMING only works on problems with OPTIMAL SUBSTRUCTURE & OVERLAPPING SUB-PROBLEMS.
+
+//  WHAT IS OVERLAPPING SUB-PROBLEMS? A problem is said to have overlapping sub-problems if it can be broken down into sub-problems which are re used several times. This means that we can break one problem down into smaller pieces, but some of those pieces are re-used. A good EXAMPLE would be FIBONACCI SEQUENCE, it does have overlapping sub problems. 
+
+
