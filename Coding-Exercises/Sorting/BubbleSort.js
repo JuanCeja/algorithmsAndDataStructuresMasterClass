9a053bdb7b88aa7f03474d062ee042483b92161e
JuanCeja <jmc347ls2@yahoo.com>
2022-12-29 14:46:14 -0800

    initial commit

---
+++ Coding-Exercises/Sorting/BubbleSort.js
@@ -0,0 +1,24 @@
+// Bubble Sort
+
+// ATTEMPT THIS IS YOU ARE UP FOR IT! Implement a function called bubbleSort. Given an array, bubbleSort will sort the values in the array. The function takes 2 parameters: an array and an optional comparator function.
+
+// The comparator function is a callback that will take two values from the array to be compared. The function returns a negative value if the first value is less than the second, a positive value if the first value is greater than the second, and 0 if both values are equal.
+
+// The default comparator you provide should assume that the two parameters are numbers and that we are sorting the values from smallest to largest.
+
+// Bubble sort is an O(n^2) algorithm.
+
+// bubbleSort([4, 20, 12, 10, 7, 9]); // [4, 7, 9, 10, 12, 20]
+// bubbleSort([0, -10, 7, 4]); // [-10, 0, 4, 7]
+// bubbleSort([1, 2, 3]); // [1, 2, 3]
+// bubbleSort([]);
+
+// ===============================================================================================================================================================================================================================
+
+function bubbleSort(arr) {
+    
+};
+
+console.log(bubbleSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
+console.log(bubbleSort([0, -10, 7, 4])); // [-10, 0, 4, 7]
+console.log(bubbleSort([1, 2, 3])); // [1, 2, 3]
