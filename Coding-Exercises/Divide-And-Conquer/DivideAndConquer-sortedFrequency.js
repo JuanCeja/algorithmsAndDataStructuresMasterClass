5b956689c62695c9a9964d1753116d2bdeab4b44
JuanCeja <jmc347ls2@yahoo.com>
2022-12-29 12:59:28 -0800

    initial commit

--- Coding-Exercises/Divide-And-Conquer/DivideAndConquer-sortedFrequency.js
+++ Coding-Exercises/Divide-And-Conquer/DivideAndConquer-sortedFrequency.js
@@ -13,7 +13,6 @@
 function sortedFrequency(arr, number) {
     let left = 0;
     let right = arr.length - 1;
-    let newArray;
 
     while(left < right) {
         if(arr[left] < number) left++;
@@ -25,7 +24,7 @@ function sortedFrequency(arr, number) {
 };
 
 
-console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2)) // 4 
-console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 3)) // 1 
-console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 1)) // 2 
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2)) // 4
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 3)) // 1
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 1)) // 2
 console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 4)) // -1
