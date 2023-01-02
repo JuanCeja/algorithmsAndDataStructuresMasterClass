2e133c8573d7b06b69a557a0bde893eb7c5ab3a9
JuanCeja <jmc347ls2@yahoo.com>
2023-01-02 15:10:58 -0800

    psuedo code for selectiont sort

--- Coding-Exercises/Sorting/SelectionSort.js
+++ Coding-Exercises/Sorting/SelectionSort.js
@@ -53,7 +53,17 @@
 // ==================================================================================================================================================================================
 
 function selectionSort(arr) {
-    
+    // store the first element as the smallest value you've seen so far
+
+    // compare this item to the next item in the array until you find a smaller number
+
+    // if a smaller number is found, designate that smaller number to be the new minimum and continue until the end of the array
+
+    // if the minimum is not the value(index) you initially began with, swap the two values
+
+    // repeat this with the next element until array is sorted
+
+    // return our sorted array
 };
 
 console.log(selectionSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
