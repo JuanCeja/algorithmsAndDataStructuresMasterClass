0737eb8cae47e865edc51ef73252f3a0a8975326
JuanCeja <jmc347ls2@yahoo.com>
2023-01-02 16:38:10 -0800

    pseudo code for insertion sort

--- Coding-Exercises/Sorting/InsertionSort.js
+++ Coding-Exercises/Sorting/InsertionSort.js
@@ -19,8 +19,14 @@
 
 // =================================================================================================================================================================================
 
-function insertionSort() {
-    
+function insertionSort(arr) {
+    // start by picking the second element in the array
+
+    // now compare the second element with the one before it and swap if necessary
+
+    // continue to the next element and if it is in the incorrect order, iterate through the sorted portion(i.e the left side) to place the element in the correct place
+
+    // return our arr
 };
 
 console.log(insertionSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
