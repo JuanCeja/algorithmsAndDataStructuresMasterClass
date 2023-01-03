8d67839fc173ed9dcb90e1fc0ab628792462a7c3
JuanCeja <jmc347ls2@yahoo.com>
2023-01-02 16:31:21 -0800

    initial commit

---
+++ Coding-Exercises/Sorting/InsertionSort.js
@@ -0,0 +1,29 @@
+// Insertion Sort
+
+// Implement insertionSort. Given an array, both algorithms will sort the values in the array. The functions take 2 parameters: an array and an optional comparator function. The comparator function is a callback that will take two values from the array to be compared. The function returns a negative value if the first value is less than the second, a positive value if the first value is greater than the second, and 0 if both values are equal. The default comparator you provide should assume that the two parameters are numbers and that we are sorting the values from smallest to largest.
+
+// Insertion Sort
+
+// Here's some guidance for how insertion sort should work:
+
+// Start by picking the second element in the array (we will assume the first element is the start of the "sorted" portion)
+// Now compare the second element with the one before it and swap if necessary.
+// Continue to the next element and if it is in the incorrect order, iterate through the sorted portion to place the element in the correct place.
+// Repeat until the array is sorted.Implement insertion sort. Your function should accept an array and return an array of sorted values.
+// Examples
+
+// insertionSort([4, 20, 12, 10, 7, 9]); // [4, 7, 9, 10, 12, 20]
+// insertionSort([0, -10, 7, 4]); // [-10, 0, 4, 7]
+// insertionSort([1, 2, 3]); // [1, 2, 3]
+// insertionSort([]);
+
+// =================================================================================================================================================================================
+
+function insertionSort() {
+    
+};
+
+console.log(insertionSort([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
+console.log(insertionSort([0, -10, 7, 4])); // [-10, 0, 4, 7]
+console.log(insertionSort([1, 2, 3])); // [1, 2, 3]
+console.log(insertionSort([]));
