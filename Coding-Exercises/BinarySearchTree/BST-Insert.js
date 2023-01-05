21c23cd867fc93acbcf3f563494a41cd563bcd54
JuanCeja <jmc347ls2@yahoo.com>
2023-01-05 14:37:52 -0800

    pseudo code for BST insert

--- Coding-Exercises/BinarySearchTree/BST-Insert.js
+++ Coding-Exercises/BinarySearchTree/BST-Insert.js
@@ -19,7 +19,19 @@ class BinarySearchTree {
         this.root = null;
     }
     insert(value) {
-        
+        let newNode  = new Node(value);
+        if(this.root === null) this.root = newNode;
+        // if there is a root, check if the value of the new node is greater than or less than the value of the root
+
+            // if it is greater
+                // check to see if there is a node to the right
+                    // if there is, move to that node and repeat these steps
+                    // if there is not, add that node as the right property and return tree
+                
+            // if it is less
+                // check to see if there is a node to the left
+                    // if there is, move to that node and repeat these steps
+                    // if there is not, add that node as the left property and return tree
     }
 }
 
