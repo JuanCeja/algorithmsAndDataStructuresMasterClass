a85ed6f4766d874b253c4d30a8e2580a2ed086c4
JuanCeja <jmc347ls2@yahoo.com>
2023-01-09 13:14:00 -0800

    pseudo code for DFS preOrder

--- Coding-Exercises/BinarySearchTree/DFS-PreOrder.js
+++ Coding-Exercises/BinarySearchTree/DFS-PreOrder.js
@@ -46,7 +46,21 @@ class BinarySearchTree {
         }
     }
     depthFirstSearchPreOrder() {
+        // create a variable to store the values of nodes visited
 
+        // store the root of the BST in a variable called current
+
+        // write a helper function which accepts a node
+
+            // push the value of the node to the variable that stores the values
+
+            // if the node has a left property, call the helper function with the left property on the node
+
+            // if the node has a right property, call the helper function with the right property on the node
+
+        // invoke the helper function with the current variable
+
+        // return the array of values
     }
 }
 
