6ff3b27e61e7dc23fce7ebc907fd581720a83b94
JuanCeja <jmc347ls2@yahoo.com>
2023-01-10 13:45:48 -0800

    initial commit

---
+++ Coding-Exercises/Binary-Heap/Insert.js
@@ -0,0 +1,27 @@
+// BinaryHeap - insert Exercise
+
+// Implement the following functions on the maxBinaryHeap class
+
+// insert
+
+// Implement the `insert` function on the `MaxBinaryHeap.prototype`: This function should insert a node in a binary heap. Make sure to re-order the heap after insertion if necessary.
+
+// ============================================================================================================================================================================================
+
+class MaxBinaryHeap {
+    constructor() {
+        this.values = [];
+    }
+    insert() {
+
+    }
+}
+
+binaryHeap = new MaxBinaryHeap()
+binaryHeap.insert(1)
+binaryHeap.insert(2)
+binaryHeap.insert(3)
+binaryHeap.insert(4)
+binaryHeap.insert(5)
+binaryHeap.insert(6)
+console.log(binaryHeap.values) // [6, 4, 5, 1, 3, 2]
