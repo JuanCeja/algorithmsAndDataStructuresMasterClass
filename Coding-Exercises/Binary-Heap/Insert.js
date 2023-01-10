f5cfcae16938dd312ad618d9a83af125d25e5ac4
JuanCeja <jmc347ls2@yahoo.com>
2023-01-10 14:00:02 -0800

    pseudo code for insert in binary heap

--- Coding-Exercises/Binary-Heap/Insert.js
+++ Coding-Exercises/Binary-Heap/Insert.js
@@ -12,8 +12,19 @@ class MaxBinaryHeap {
     constructor() {
         this.values = [];
     }
-    insert() {
+    insert(value) {
+        // push the value into the values property on the heap
 
+        // bubble up
+            // create a variable called index which is the length of the values property - 1
+
+            // create a variable called parentIndex which is the floor of (index - 1) / 2
+
+            // keep looping as long as the values element at the parentIndex is less than the values element at the child index
+
+                // swap the value of the values element at the parentIndex with the value of the element property at the child index
+
+                // set the index to be the parentIndex, and start over
     }
 }
 
