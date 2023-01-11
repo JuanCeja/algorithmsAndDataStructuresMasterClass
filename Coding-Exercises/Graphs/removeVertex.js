0df406b2b394da3d225adb42aff3a16236d5e91e
JuanCeja <jmc347ls2@yahoo.com>
2023-01-11 13:32:29 -0800

    initial commit

--- Coding-Exercises/Graphs/removeVertex.js
+++ Coding-Exercises/Graphs/removeVertex.js
@@ -1,8 +1,8 @@
-// Graphs Exercise - removeEdge
+// Graphs Exercise - removeVertex
 
-// Implement the following methods on the Graph class, addEdge and addVertex have been implemented for you.
+// Implement the following methods on the Graph class, removeEdge has been implemented for you.
 
-// removeEdge - this function should accept two nodes and remove the edge between them. It should modify the adjacency list to ensure that both values are not in each array for the two nodes which no longer contain the edge.
+// removeVertex - this function should remove the node in the array of nodes and also remove all edges that the removed node previously contained.
 
 // ============================================================================================================================================================================================
 
@@ -19,13 +19,9 @@ class Graph {
         this.adjacencyList[vertex2].push(vertex1);
     }
 
-    removeEdge (vertex1, vertex2) {
+    removeEdge(vertex1, vertex2) {
         this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(element => element !== vertex2);
         this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(element => element !== vertex1);
-    }
-
-    removeVertex(vertex){
-        
     }
 }
 
@@ -39,9 +35,3 @@ graph.addEdge('A', 'B');
 graph.addEdge('A', 'C');
 graph.addEdge('B', 'D');
 graph.addEdge('C', 'D');
-graph.removeEdge('B', 'A');
-graph.removeEdge('C', 'D');
-console.log(graph.adjacencyList['A']); // contains 'C'
-console.log(graph.adjacencyList['B']); // contains 'D'
-console.log(graph.adjacencyList['C']); // contains 'A'
-console.log(graph.adjacencyList['D']); // contains 'B'
