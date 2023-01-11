e5333783032ef7eeca148ef61ff797e0e55bcad5
JuanCeja <jmc347ls2@yahoo.com>
2023-01-11 15:06:49 -0800

    complete

--- Coding-Exercises/Graph-Traversal/DFS.js
+++ Coding-Exercises/Graph-Traversal/DFS.js
@@ -10,37 +10,40 @@ class Graph {
     constructor() {
         this.adjacencyList = {};
     }
+
     addVertex(vertex) {
-        this.adjacencyList[vertex] = [];
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
-    addEdge(vertex1, vertex2) {
-        this.adjacencyList[vertex1].push(vertex2);
-        this.adjacencyList[vertex2].push(vertex1);
-    }
+
+    addEdge(v1, v2) {
+        if (this.adjacencyList[v1]) this.adjacencyList[v1].push(v2);
+        if (this.adjacencyList[v2]) this.adjacencyList[v2].push(v1);
+    };
+
     removeEdge(vertex1, vertex2) {
-        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
-            v => v !== vertex2
-        );
-        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
-            v => v !== vertex1
-        );
-    }
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+    };
+
     depthFirstSearch(start) {
-        // let S be a stack
-
-        // S push start
-
-        // while S is not empty
-
-            // vertex = S.pop()
-
-            // if vertex is not labeled as discovered:
-
-                // visit vertex(add to result list)
-
-                // label vertex as discovered
-
-                // for each of vertex's neighbors, N do S.push(N)
+        const stack = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+        
+        visited[start] = true;
+        while(stack.length) {
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+            
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            });
+        }
+        return result;
     }
 }
 
@@ -72,7 +75,7 @@ graph.addEdge('V', 'W');
 graph.addEdge('R', 'T');
 graph.addEdge('W', 'T');
 
-graph.depthFirstSearch('S');
+console.log(graph.depthFirstSearch('S'));
 
 /**
  * results:
