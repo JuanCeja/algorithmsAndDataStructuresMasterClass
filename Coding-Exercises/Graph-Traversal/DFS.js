0818b6f59cbe8dcc13c70e5cf1e5934c0600d6f5
JuanCeja <jmc347ls2@yahoo.com>
2023-01-11 15:07:21 -0800

    complete

--- Coding-Exercises/Graph-Traversal/DFS.js
+++ Coding-Exercises/Graph-Traversal/DFS.js
@@ -30,14 +30,14 @@ class Graph {
         const result = [];
         const visited = {};
         let currentVertex;
-        
+
         visited[start] = true;
-        while(stack.length) {
+        while (stack.length) {
             currentVertex = stack.pop();
             result.push(currentVertex);
-            
+
             this.adjacencyList[currentVertex].forEach(neighbor => {
-                if(!visited[neighbor]) {
+                if (!visited[neighbor]) {
                     visited[neighbor] = true;
                     stack.push(neighbor);
                 }
