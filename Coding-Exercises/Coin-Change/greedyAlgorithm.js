9436af97b0ed948efe70077b032b821724d17ee7
JuanCeja <jmc347ls2@yahoo.com>
2023-01-12 15:41:57 -0800

    added step by step comments

--- Coding-Exercises/Coin-Change/greedyAlgorithm.js
+++ Coding-Exercises/Coin-Change/greedyAlgorithm.js
@@ -4,30 +4,30 @@
 
 // ============================================================================================================================================================================================
 
-function minCoinChange(coins, amount) {
-    const arr_min = []
+function minCoinChange(denominations, value) {
+    // Initialize an array to store the minimum number of coins for each value
+    const minCoins = new Array(value + 1).fill(Infinity);
+    // Initialize an array to store the coins used for each value
+    const coins = new Array(value + 1);
+    // The minimum number of coins to obtain a value of 0 is 0
+    minCoins[0] = 0;
 
-    for (let i = 0; i < amount + 1; i++) {
-        arr_min.push(i)
-    }
-
-    function helper(amount, arr_min) {
-        if (coins.includes(amount)) {
-            return [amount]
-        }
-
-        let result;
-        for (const coin of coins) {
-            if (coin <= amount) {
-                result = [coin].concat(helper(amount - coin, arr_min))
-                if (result.length < arr_min[amount]) {
-                    arr_min[amount] = result.length
-                }
+    // Iterate through each denomination
+    for (let i = 0; i < denominations.length; i++) {
+        // Iterate through each value from the denomination to the target value
+        for (let j = denominations[i]; j <= value; j++) {
+            // If using the current denomination results in fewer coins than the current minimum
+            if (minCoins[j - denominations[i]] + 1 < minCoins[j]) {
+                // Update the minimum number of coins for the current value
+                minCoins[j] = minCoins[j - denominations[i]] + 1;
+                // Update the coins used for the current value
+                coins[j] = [denominations[i], ...(coins[j - denominations[i]] || [])];
             }
         }
-        return result
     }
-    return helper(amount, arr_min)
+
+    // Return the coins used to obtain the target value
+    return coins[value];
 }
 
 const denominations = [1, 5, 10, 25]
