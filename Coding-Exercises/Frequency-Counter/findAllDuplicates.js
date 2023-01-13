b5ea539293dcd2b905e72076105ae8cb67e2627c
JuanCeja <jmc347ls2@yahoo.com>
2023-01-13 13:20:08 -0800

    complete

--- Coding-Exercises/Frequency-Counter/findAllDuplicates.js
+++ Coding-Exercises/Frequency-Counter/findAllDuplicates.js
@@ -9,10 +9,20 @@
 
 // ============================================================================================================================================================================================
 
-function findAllDuplicates() {
-    
+function findAllDuplicates(arr) {
+    let specialNumbers = new Set();
+    let result = [];
+
+    for(let num of arr) {
+        if(!specialNumbers.has(num)) {
+            specialNumbers.add(num);
+        } else {
+            result.push(num);
+        }
+    }
+    return result;
 }
 
-findAllDuplicates([4, 3, 2, 7, 8, 2, 3, 1]) // array with 2 and 3
-findAllDuplicates([4, 3, 2, 1, 0]) // []
-findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3]) // array with 3, 2, and 1
+console.log(findAllDuplicates([4, 3, 2, 7, 8, 2, 3, 1])) // array with 2 and 3
+console.log(findAllDuplicates([4, 3, 2, 1, 0])) // []
+console.log(findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3])) // array with 3, 2, and 1
