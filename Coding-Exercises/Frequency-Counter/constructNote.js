35caa300cf26538f74f9c693df18e688ba2bb9ff
JuanCeja <jmc347ls2@yahoo.com>
2023-01-12 16:22:16 -0800

    complete

--- Coding-Exercises/Frequency-Counter/constructNote.js
+++ Coding-Exercises/Frequency-Counter/constructNote.js
@@ -16,13 +16,29 @@
 
 function constructNote(letters, message) {
     // create 2 objects to compare at the end
+    let lettersObj = {};
+    let messageObj = {};
 
     // loop through both strings to create the objects
+    for (let i = 0; i < letters.length; i++) {
+        if(!lettersObj[letters[i]]) lettersObj[letters[i]] = 1;
+        else lettersObj[letters[i]]++;
+    }
+
+    for (let j = 0; j < message.length; j++) {
+        if(!messageObj[message[j]]) messageObj[message[j]] = 1;
+        else messageObj[message[j]]++;
+    }
 
     // loop through one of the objects and compare the values to the other object
+    for (const value in lettersObj) {
         // if the values dont match return false
+        if(lettersObj[value] !== messageObj[value]) return false;
+    }
+
 
     // return true
+    return true;
 }
 
 console.log(constructNote('aa', 'abc')) // false
