c8ecaf1844b28f2b8dd953bb5795cb1f9f7d444c
JuanCeja <jmc347ls2@yahoo.com>
2023-01-13 13:56:45 -0800

    initial commit

---
+++ Coding-Exercises/trie/addWord.js
@@ -0,0 +1,12 @@
+// Trie Exercise - addWord
+
+// This function should add the given word starting from the given index to the Trie.
+
+// It will be recursive and notify the correct child of this Trie to add the word starting from a later index.
+
+// Consider what the add function should do when it reaches the end of the word as a word does not necessarily end at a leaf.
+
+// You must mark nodes which are the ends of words so that the words can be reconstructed later.
+
+// ============================================================================================================================================================================================
+
